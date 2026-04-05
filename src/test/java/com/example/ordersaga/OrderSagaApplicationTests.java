0152b9649fdc7c05d46bd9bb3c9481c9763aeae1
leeyoungkon <yklee2002@gmail.com>
2026-04-05 16:33:19 +0900

    chore: separate order-saga test package

--- src/test/java/com/example/ordersaga/OrderSagaApplicationTests.java
+++ src/test/java/com/example/ordersaga/OrderSagaApplicationTests.java
@@ -1,13 +1,12 @@
-package com.example.stocksaga;
+package com.example.ordersaga;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class StockSagaApplicationTests {
+class OrderSagaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
